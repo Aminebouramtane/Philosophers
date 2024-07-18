6b08c2ca1de7bd55d3b07f64ca5a51eb873e1670
Aminebouramtane <bouramtaneamine1@gmail.com>
2024-07-18 21:19:28 +0100

    some changges

--- philo/philo.c
+++ philo/philo.c
@@ -2,25 +2,37 @@
 
 #include "philo.h"
 
+void ft_usleep(size_t time)
+{
+    size_t newtime;
+    newtime = ft_gettime();
+    while (ft_gettime()- newtime < time);
+}
+
+void    ft_eating(t_philo *philo, t_table *table)
+{
+        pthread_mutex_lock(philo->right_fork);
+        printf("%zu %d has taken a fork\n", (ft_gettime() - table->start_at), philo->philo_id);
+        pthread_mutex_lock(philo->left_fork);
+        philo->last_meal = ft_gettime();
+        printf("%zu %d has taken a fork\n", (ft_gettime() - table->start_at), philo->philo_id);
+        printf("%zu %d is eating\n", (ft_gettime() - table->start_at), philo->philo_id);
+        ft_usleep(table->time_to_eat);//
+        pthread_mutex_unlock(philo->left_fork);
+        pthread_mutex_unlock(philo->right_fork);
+        printf("%zu %d is sleeping\n", (ft_gettime() - table->start_at), philo->philo_id);
+        ft_usleep(table->time_to_sleep);
+        printf("%zu %d is thinking\n", (ft_gettime() - table->start_at), philo->philo_id);
+}
+
+
 void    *routine(void *philos)
 {
     t_philo *philo = (t_philo *) philos;
     t_table *table = philo->table;
     while (1)
     {
-        pthread_mutex_lock(philo->left_fork);
-        printf("%zu ms, %d token left fork\n", (ft_gettime() - table->start_at), philo->philo_id);
-        pthread_mutex_lock(philo->right_fork);
-        printf("%zu ms, %d token right fork\n", (ft_gettime() - table->start_at), philo->philo_id);
-        printf("%zu ms, %d is eating\n", (ft_gettime() - table->start_at), philo->philo_id);
-        philo->last_meal = ft_gettime();
-        usleep(table->time_to_eat * 1000);
-        pthread_mutex_unlock(philo->left_fork);
-        pthread_mutex_unlock(philo->right_fork);
-        printf("%zu ms, %d is sleeping\n", (ft_gettime() - table->start_at), philo->philo_id);
-        usleep(table->time_to_sleep * 1000);
-        printf("%zu ms, %d is thinking\n", (ft_gettime() - table->start_at), philo->philo_id);
-
+        ft_eating(philo, table);
     }
     return (0);
 }
@@ -44,29 +56,27 @@ int     init_philo(t_table *table, t_philo *philo)
             philo[i].right_fork = &table->forks[i + 1];
     }
 
-    // i = -1;
-    // while (++i < table->nb_of_philo)
-    // {
-    //     printf("--->%d\n", philo[i].meals_counter);
-    //     printf("--->%d\n", philo[i].if_full);
-    //     printf("--->%zu\n", philo[i].last_meal);
-    //     printf("=======================================\n");
-    // }
     return (0);
 }
 
 void    ft_monitor(t_table *table, t_philo *philo)
 {
-    size_t i = 0;
+    size_t i;
+    size_t time;
     while (1)
     {
+        i = 0;
+        // if (philo->philo_id % 2 != 0)
+        //     usleep(500);
         while (i < table->nb_of_philo)
         {
-            if (ft_gettime() - philo[i].last_meal < table->time_to_die)
+            time = ft_gettime();
+            if (time - philo[i].last_meal > table->time_to_die)
             {
-                printf("%zu ms, %d is die\n", (ft_gettime() - table->start_at), philo[i].philo_id);
+                printf("%zu %d died\n", (time - table->start_at), philo[i].philo_id);
                 exit (0);
             }
+            i++;
         }
     }
 }
@@ -86,7 +96,7 @@ void    create_pthreads(t_table *table)
         pthread_create(&philo[i].philo, NULL, &routine, &philo[i]);
         usleep(20);
     }
-    ft_monitor(table, philo);
+    ft_monitor(table, philo); 
     i = -1;
     while (++i < table->nb_of_philo)
         pthread_join(philo[i].philo, NULL);
